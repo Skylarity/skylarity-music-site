3744aeae019b470263d4d1234372b936d10e90a7
Skyler Rexroad <skyrex1095@gmail.com>
2015-08-28 18:41:11 -0600

    NUKED IT

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+$CURRENT_DIR = __DIR__;
+$PAGE_TITLE = "Skyler Rexroad";
+?>
+<?php require_once("php/lib/head-utils.php"); ?>
+	<body>
+		<?php require_once("php/lib/header.php"); ?>
+	</body>
+<?php require_once("php/lib/foot-utils.php"); ?>
---
+++ php/lib/header.php
@@ -0,0 +1,19 @@
+<header class="header">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-4">
+				<h1>SKYLER REXROAD</h1>
+			</div>
+			<div class="col-md-4 col-md-offset-4">
+				<nav class="nav nav-pills pull-right">
+					<li role="presentation" class="active">
+						<a href="#">Home</a>
+					</li>
+					<li role="presentation">
+						<a href="#">Contact</a>
+					</li>
+				</nav>
+			</div>
+		</div>
+	</div>
+</header>
