68d77cd97feab1c6a2b2c0ceb9358c5f8ed9e8d6
Skyler Rexroad <skyrex1095@gmail.com>
2015-08-30 15:37:49 -0600

    Made music page

--- php/lib/content.php
+++ php/lib/content.php
@@ -1,29 +1,28 @@
 <div class="content">
     <div class="container">
         <div class="row">
-            <div class="col-md-8">
-                <h2 class="content-h2">Mission Statement</h2>
+            <div class="col-md-4">
+                <h2 class="content-h2">Check out my music</h2>
 
                 <p>
-                    This is a mission statement.
+                    This is a small explanation about my music.
                 </p>
             </div>
-            <div class="col-md-4">
-                <img class="img-responsive center-block content-img" src="<?php echo $PREFIX; ?>images/robot-blue.png"
-                     alt="Mission statement"/>
+            <div class="col-md-8">
+                <iframe width="100%" height="350" scrolling="no" frameborder="no"
+                        src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/users/8186758&amp;color=508cff&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false"></iframe>
             </div>
         </div>
         <div class="row">
-            <div class="col-md-4">
-                <img class="img-responsive center-block content-img" src="<?php echo $PREFIX; ?>images/robot-blue.png"
-                     alt="Mission statement"/>
-            </div>
             <div class="col-md-8">
+                GALLERY GOES HERE
+            </div>
+            <div class="col-md-4">
                 <div class="pull-right">
-                    <h2 class="content-h2">My Workflow</h2>
+                    <h2 class="content-h2">Check out some websites I've done</h2>
 
                     <p>
-                        This is an explanation of my workflow.
+                        This is an explanation about websites I've made.
                     </p>
                 </div>
             </div>
