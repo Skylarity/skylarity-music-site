98b4ddc51ab4c0dd4bb16685bb4e7536b7438f2b
Skyler Rexroad <skyrex1095@gmail.com>
2015-08-31 07:57:21 -0600

    Made logo link to homepage

--- php/lib/header.php
+++ php/lib/header.php
@@ -1,25 +1,24 @@
 <header class="header">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-6">
-                <img class="img-responsive logo-robot pull-left" src="<?php echo $PREFIX; ?>images/robot-blue.png"
-                     alt="Robot Logo"/>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-6">
+				<a href="<?php echo $PREFIX; ?>index.php">
+					<img class="img-responsive logo-robot pull-left" src="<?php echo $PREFIX; ?>images/robot-blue.png"
+						 alt="Robot Logo"/>
 
-                <h2 class="logo">SKYLER REXROAD</h2>
-            </div>
-            <div class="col-md-6">
-                <nav class="nav nav-pills pull-right header-nav">
-                    <li role="presentation">
-                        <a href="<?php echo $PREFIX; ?>index.php">Home</a>
-                    </li>
-                    <li role="presentation">
-                        <a href="<?php echo $PREFIX; ?>music">Music</a>
-                    </li>
-                    <li role="presentation">
-                        <a href="<?php echo $PREFIX; ?>websites">Websites</a>
-                    </li>
-                </nav>
-            </div>
-        </div>
-    </div>
+					<h2 class="logo">SKYLER REXROAD</h2>
+				</a>
+			</div>
+			<div class="col-md-6">
+				<nav class="nav nav-pills pull-right header-nav">
+					<li role="presentation">
+						<a href="<?php echo $PREFIX; ?>music">Music</a>
+					</li>
+					<li role="presentation">
+						<a href="<?php echo $PREFIX; ?>websites">Websites</a>
+					</li>
+				</nav>
+			</div>
+		</div>
+	</div>
 </header>
