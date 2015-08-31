faf0df896746d657aa99281917f7d44a34b00785
Skyler Rexroad <skyrex1095@gmail.com>
2015-08-30 18:22:32 -0600

    Placeholder websites page

---
+++ php/lib/websites.php
@@ -0,0 +1,15 @@
+<div class="websites">
+    <div class="websites-bg animated fadeIn">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <h1 class="websites-h1">
+                        Nothing to see here...
+                        <br/>
+                        <small>(Eventually you will see websites)</small>
+                    </h1>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
