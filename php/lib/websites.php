46a16769910c174b47aaa60da67ec66232be5a60
Skyler Rexroad <skyrex1095@gmail.com>
2015-08-31 16:28:01 -0600

    Added swipebox

--- php/lib/websites.php
+++ php/lib/websites.php
@@ -1,15 +1,45 @@
 <div class="websites">
-    <div class="websites-bg animated fadeIn">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-12">
-                    <h1 class="websites-h1">
-                        Nothing to see here...
-                        <br/>
-                        <small>(Eventually you will see websites)</small>
-                    </h1>
-                </div>
-            </div>
-        </div>
-    </div>
+	<div class="websites-bg animated fadeIn">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<h1 class="websites-h1">
+						Nothing to see here...
+						<br/>
+						<small>(Eventually you will see websites)</small>
+					</h1>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-3 col-md-offset-3">
+				<a class="box" href="<?php echo $PREFIX; ?>images/big/robot-blue.png" class="swipebox"
+				   title="My Caption">
+					<img src="<?php echo $PREFIX; ?>images/robot-blue.png" alt="image">
+				</a>
+			</div>
+			<div class="col-md-3 col-md-offset-3">
+				<a class="box" href="<?php echo $PREFIX; ?>images/big/robot-blue.png" class="swipebox"
+				   title="My Caption">
+					<img src="<?php echo $PREFIX; ?>images/robot-blue.png" alt="image">
+				</a>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-3 col-md-offset-3">
+				<a class="box" href="<?php echo $PREFIX; ?>images/big/robot-blue.png" class="swipebox"
+				   title="My Caption">
+					<img src="<?php echo $PREFIX; ?>images/robot-blue.png" alt="image">
+				</a>
+			</div>
+			<div class="col-md-3 col-md-offset-3">
+				<a class="box" href="<?php echo $PREFIX; ?>images/big/robot-blue.png" class="swipebox"
+				   title="My Caption">
+					<img src="<?php echo $PREFIX; ?>images/robot-blue.png" alt="image">
+				</a>
+			</div>
+		</div>
+	</div>
 </div>
