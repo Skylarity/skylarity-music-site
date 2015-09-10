7ff8137dbd80d8881005448d310856b87d26a4d3
Skyler Rexroad <skyrex1095@gmail.com>
2015-09-09 21:27:38 -0600

    Updated call to action text

--- php/lib/call-to-action.php
+++ php/lib/call-to-action.php
@@ -6,7 +6,7 @@
                     <h1>
                         Hi, I'm Skyler.
                         <br/>
-                        <small>Musician, web developer.</small>
+                        <small>Musician, web developer, programmer.</small>
                         <br/>
                         <small>Go ahead, take a scroll.</small>
                     </h1>
