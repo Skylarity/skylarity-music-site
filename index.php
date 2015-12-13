b3953919adc12ac1ec1499bf204da6838ff4a99c
Skyler Rexroad <skyrex1095@gmail.com>
2015-12-12 23:54:10 -0700

    Updated index with "coming soon message", and updated image names

--- index.php
+++ index.php
@@ -3,10 +3,13 @@ $CURRENT_DIR = __DIR__;
 $PAGE_TITLE = "Skylarity";
 ?>
 <?php require_once("php/lib/head-utils.php"); ?>
-    <body class="sfooter">
-        <div class="sfooter-content">
-            <?php require_once("php/lib/header.php"); ?>
-        </div>
-        <?php require_once("php/lib/footer.php"); ?>
-    </body>
+	<body class="sfooter">
+		<div class="sfooter-content">
+			<?php require_once("php/lib/header.php"); ?>
+			<div class="container">
+				<h1>Content coming soon.</h1>
+			</div>
+		</div>
+		<?php require_once("php/lib/footer.php"); ?>
+	</body>
 <?php require_once("php/lib/foot-utils.php"); ?>
