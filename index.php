0f7fa2ee5bf6e57ee1c1a57688e65acfc02e78a9
Skyler Rexroad <skyrex1095@gmail.com>
2015-12-12 23:30:15 -0700

    Nuked most of old site, added new favicon.

--- index.php
+++ index.php
@@ -1,15 +1,11 @@
 <?php
 $CURRENT_DIR = __DIR__;
-$PAGE_TITLE = "Skyler Rexroad";
+$PAGE_TITLE = "Skylarity";
 ?>
 <?php require_once("php/lib/head-utils.php"); ?>
     <body class="sfooter">
         <div class="sfooter-content">
             <?php require_once("php/lib/header.php"); ?>
-
-            <?php require_once("php/lib/call-to-action.php"); ?>
-
-            <?php require_once("php/lib/content.php"); ?>
         </div>
         <?php require_once("php/lib/footer.php"); ?>
     </body>
