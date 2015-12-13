2263ec70cdd0a071325d3ab9ab335a6d40de4f1e
Skyler Rexroad <skyrex1095@gmail.com>
2015-12-13 11:43:24 -0700

    Update blurb typography

--- index.php
+++ index.php
@@ -8,22 +8,21 @@ $PAGE_TITLE = "Skylarity";
 		<div class="container">
 			<div class="row content-pad">
 				<div class="col-md-6">
-					<h2 class="typewriter-text">Who?</h2>
 					<p>
-						Skylarity is a producer from New Mexico. He started out making chiptune music in PXTone, and graduated
-						to FL Studio when he realized his love for bass music.
+						<span class="lead typewriter-text">Skylarity</span> is a producer from New Mexico. He started out
+						making chiptune music in PXTone, and graduatedto FL Studio when he realized his love for bass music.
 					</p>
 				</div>
 				<div class="col-md-6">
-					<h2 class="typewriter-text"></h2>
 					<p>
-
+						<span class="lead typewriter-text">Skylarity</span> primarily uses FL Studio to make bass music, but
+						he also likes indie rock, and makes chiptunes with LSDJ every now and then.
 					</p>
 				</div>
 			</div>
 			<div class="row content-pad">
 				<div class="col-md-12">
-					<iframe width="100%" height="350" scrolling="no" frameborder="no"
+					<iframe width="100%" height="500" scrolling="no" frameborder="no"
 							  src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/users/8186758&amp;color=508cff&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false"></iframe>
 				</div>
 			</div>
